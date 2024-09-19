34d3a2ac30fe4d36662779aa0301aedc332cf4e1
Abhishek Kumar <98731119+thekrabhishek@users.noreply.github.com>
2024-09-20 00:49:09 +0530

    localstorage

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
-const todoList = [];
-  
-  renderTodoList();
-  
-  function renderTodoList() {
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+renderTodoList();
+
+function renderTodoList() {
     let todoListHTML = '';
-  
+
     for (let i = 0; i < todoList.length; i++) {
-      const todoObject = todoList[i];
-      
-      const { name, dueDate } = todoObject;
-      const html = `
-        <div>${name}</div>
-        <div>${dueDate}</div>
-        <button onclick="
-          todoList.splice(${i}, 1);
-          renderTodoList();
-        " class="delete-todo-button">Delete</button> 
-      `;
-      todoListHTML += html;
+        const todoObject = todoList[i];
+
+        const { name, dueDate } = todoObject;
+        const html = `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button onclick="
+                deleteTodo(${i});
+            " class="delete-todo-button">Delete</button> 
+        `;
+        todoListHTML += html;
     }
-  
+
     document.querySelector('.js-todo-list')
-      .innerHTML = todoListHTML;
-  }
-  
-  function addTodo() {
+        .innerHTML = todoListHTML;
+}
+
+function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value.trim();
-  
+
     const dateInputElement = document.querySelector('.js-due-date-input');
     const dueDate = dateInputElement.value;
+
     if (name === '' || dueDate === '') {
         alert('Please enter both task name and due date.');
         return;
-      }
-  
+    }
+
     todoList.push({
-     
-      name,
-      dueDate
+        name,
+        dueDate
     });
-  
+
     inputElement.value = '';
-  
+
+    saveToLocalStorage();
     renderTodoList();
-  }
+}
+
+function deleteTodo(index) {
+    todoList.splice(index, 1);
+    saveToLocalStorage();
+    renderTodoList();
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
